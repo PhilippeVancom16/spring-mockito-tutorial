03672740ce5df70ff755dfdc01f3fddf43cdae4c
Phil <philippe.vancom@gmail.com>
2019-11-14 13:43:10 +0100

    new dump

--- db.sql
+++ db.sql
@@ -16,37 +16,48 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- Name: mockito; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE mockito WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
-
-
-ALTER DATABASE mockito OWNER TO postgres;
-
-\connect mockito
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
 SET default_tablespace = '';
 
 SET default_with_oids = false;
 
 --
--- Name: user; Type: TABLE; Schema: public; Owner: postgres
+-- Name: company; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public."user" (
+CREATE TABLE public.company (
+    company_id bigint NOT NULL,
+    company_name character varying NOT NULL
+);
+
+
+ALTER TABLE public.company OWNER TO postgres;
+
+--
+-- Name: company_company_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.company_company_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.company_company_id_seq OWNER TO postgres;
+
+--
+-- Name: company_company_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.company_company_id_seq OWNED BY public.company.company_id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
     user_id bigint NOT NULL,
     user_firstname character varying NOT NULL,
     user_lastname character varying NOT NULL,
@@ -54,7 +65,7 @@ CREATE TABLE public."user" (
 );
 
 
-ALTER TABLE public."user" OWNER TO postgres;
+ALTER TABLE public.users OWNER TO postgres;
 
 --
 -- Name: user_user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
@@ -74,25 +85,48 @@ ALTER TABLE public.user_user_id_seq OWNER TO postgres;
 -- Name: user_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE public.user_user_id_seq OWNED BY public."user".user_id;
+ALTER SEQUENCE public.user_user_id_seq OWNED BY public.users.user_id;
 
 
 --
--- Name: user user_id; Type: DEFAULT; Schema: public; Owner: postgres
+-- Name: company company_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."user" ALTER COLUMN user_id SET DEFAULT nextval('public.user_user_id_seq'::regclass);
+ALTER TABLE ONLY public.company ALTER COLUMN company_id SET DEFAULT nextval('public.company_company_id_seq'::regclass);
 
 
 --
--- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Name: users user_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-INSERT INTO public."user" VALUES (1, 'Louis', 'Hella', 3);
-INSERT INTO public."user" VALUES (2, 'Philippe', 'Vancom', 3);
-INSERT INTO public."user" VALUES (3, 'Romain', 'Gerardy', 1);
-INSERT INTO public."user" VALUES (4, 'Jean', 'VanRickstal', 2);
-INSERT INTO public."user" VALUES (5, 'Benoit', 'Dormaels', 1);
+ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.user_user_id_seq'::regclass);
+
+
+--
+-- Data for Name: company; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.company VALUES (1, 'Afelio');
+INSERT INTO public.company VALUES (2, 'NRB');
+INSERT INTO public.company VALUES (3, 'Xperthis');
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.users VALUES (1, 'Louis', 'Hella', 3);
+INSERT INTO public.users VALUES (2, 'Philippe', 'Vancom', 3);
+INSERT INTO public.users VALUES (3, 'Romain', 'Gerardy', 1);
+INSERT INTO public.users VALUES (4, 'Jean', 'VanRickstal', 2);
+INSERT INTO public.users VALUES (5, 'Benoit', 'Dormaels', 1);
+
+
+--
+-- Name: company_company_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.company_company_id_seq', 3, true);
 
 
 --
@@ -103,18 +137,26 @@ SELECT pg_catalog.setval('public.user_user_id_seq', 5, true);
 
 
 --
--- Name: user user_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: company company_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."user"
+ALTER TABLE ONLY public.company
+    ADD CONSTRAINT company_pkey PRIMARY KEY (company_id);
+
+
+--
+-- Name: users user_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
     ADD CONSTRAINT user_pkey PRIMARY KEY (user_id);
 
 
 --
--- Name: user FK_company_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: users FK_company_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."user"
+ALTER TABLE ONLY public.users
     ADD CONSTRAINT "FK_company_id" FOREIGN KEY (company_id) REFERENCES public.company(company_id);
 
 
